e10c84f45e8b690bd0edc3d506b247b4fcb82de1
sunnymaurya719 <sunnymaurya1920@gmail.com>
2026-03-15 11:51:54 +0530

    .

--- server/server.js
+++ server/server.js
@@ -26,6 +26,7 @@ const allowedOrigins = [
     'http://127.0.0.1:5173',
     'http://localhost:5174',
     'http://127.0.0.1:5174',
+    'https://lavishfashion.vercel.app',
     ...envOrigins
 ]
     .map(normalizeOrigin)
